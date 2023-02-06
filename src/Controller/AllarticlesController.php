942a404d52ada3812e403a09533c01576bede258
yanis.pirlet <yanis.pirlet@gmail.com>
2023-02-06 12:50:56 +0100

    organisation et allproduit

--- src/Controller/AllarticlesController.php
+++ src/Controller/AllarticlesController.php
@@ -2,17 +2,31 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Articles;
+use App\Entity\Categories;
+use App\Repository\ArticlesRepository;
+use App\Repository\CategoriesRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AllarticlesController extends AbstractController
 {
     #[Route('/allarticles', name: 'app_allarticles')]
-    public function index(): Response
+    public function index(ArticlesRepository $articlesRepository,CategoriesRepository $categoriesRepository): Response
     {
         return $this->render('allarticles/index.html.twig', [
             'controller_name' => 'AllarticlesController',
+            'categories' => $categoriesRepository->findAll(),
+            'articles' => $articlesRepository->findAll(),
+        ]);
+    }
+    #[Route('/{id}', name: 'app_allarticles_show', methods: ['GET'])]     
+    public function show(Articles $article,Categories $categorie): Response
+    {
+        return $this->render('allarticles/show.html.twig', [
+            'categorie' => $categorie,
+            'article' => $article,
         ]);
     }
 }
